5a851cb90bad64d07a46bbc3246b37d19e12cf41
Alexandr_Lyssenko <ALexandr>
2021-02-11 16:55:43 +0600

    init: initial structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+const App = () => {
+    return (
+            <div>
+                <h1>Hello React</h1>
+            </div>
+    )
+}
+
+ReactDom.render(<App/>, document.getElementById("root"))
