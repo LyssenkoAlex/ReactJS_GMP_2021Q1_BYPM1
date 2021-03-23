88bf0d5ce1c4d24ed2780891fb6d5f2fe4ddc0f7
Alexandr_Lyssenko <ALexandr>
2021-03-23 12:49:47 +0600

    fix: change to uncontrolled

--- src/components/modals/AddMovie.jsx
+++ src/components/modals/AddMovie.jsx
@@ -1,24 +1,25 @@
 /* eslint-disable jsx-a11y/no-interactive-element-to-noninteractive-role,jsx-a11y/aria-role */
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
 
 const AddMovie = ({ onClose, movie }) => {
-  const [values, setValues] = useState({
-    title: movie.title === undefined ? "" : movie.title,
-    release_date: movie.release_date === undefined ? "" : movie.release_date,
-    poster_path: movie.poster_path === undefined ? "" : movie.poster_path,
-    genres: movie.genres === undefined ? "" : movie.genres.join(),
-    overview: movie.overview === undefined ? "" : movie.overview,
-    runtime: movie.runtime === undefined ? "" : movie.runtime,
-  });
+  const titleRef = useRef(movie.title);
+  const releaseDateRef = useRef(movie.release_date);
+  const posterPathRef = useRef(movie.poster_path);
+  const genresRef = useRef(
+    movie.genres === undefined ? "" : movie.genres.join()
+  );
+  const overviewRef = useRef(movie.overview);
+  const runtimeRef = useRef(movie.runtime);
 
   const handleChange = (event) => {
-    console.log("value: ", values);
+    console.log("titleRef: ", titleRef.current.value);
+    console.log("releaseDateRef: ", releaseDateRef.current.value);
+    console.log("posterPathRef: ", posterPathRef.current.value);
+    console.log("genresRef: ", genresRef.current.value);
+    console.log("overviewRef: ", overviewRef.current.value);
+    console.log("runtimeRef: ", runtimeRef.current.value);
     event.preventDefault();
-  };
-
-  const set = (name) => ({ target: { value } }) => {
-    setValues((oldValues) => ({ ...oldValues, [name]: value }));
   };
 
   return (
@@ -36,8 +37,8 @@ const AddMovie = ({ onClose, movie }) => {
                 type="text"
                 id="title"
                 placeholder="Enter movie title"
-                onChange={set("title")}
-                value={values.title}
+                ref={titleRef}
+                defaultValue={movie.title}
               />
             </label>
           </section>
@@ -46,9 +47,9 @@ const AddMovie = ({ onClose, movie }) => {
               <span>RELEASE DATE</span>
               <input
                 type="date"
-                onChange={set("release_date")}
                 placeholder="dd-mm-yyyy"
-                value={values.release_date}
+                ref={releaseDateRef}
+                defaultValue={movie.release_date}
               />
             </label>
           </section>
@@ -57,9 +58,9 @@ const AddMovie = ({ onClose, movie }) => {
               <span>MOVIE URL</span>
               <input
                 type="text"
-                onChange={set("poster_path")}
                 placeholder="Enter movie URL"
-                value={values.poster_path}
+                ref={posterPathRef}
+                defaultValue={movie.poster_path}
               />
             </label>
           </section>
@@ -69,8 +70,8 @@ const AddMovie = ({ onClose, movie }) => {
               <input
                 type="text"
                 placeholder="Select genre"
-                onChange={set("title")}
-                value={values.genres}
+                ref={genresRef}
+                defaultValue={movie.genres}
               />
             </label>
           </section>
@@ -80,8 +81,8 @@ const AddMovie = ({ onClose, movie }) => {
               <input
                 type="text"
                 placeholder="Overview here"
-                onChange={set("overview")}
-                value={values.overview}
+                ref={overviewRef}
+                defaultValue={movie.overview}
               />
             </label>
           </section>
@@ -91,8 +92,8 @@ const AddMovie = ({ onClose, movie }) => {
               <input
                 type="number"
                 placeholder="Runtime here"
-                onChange={set("runtime")}
-                value={values.runtime}
+                ref={runtimeRef}
+                defaultValue={movie.runtime}
                 min="0"
                 max="10000"
               />
