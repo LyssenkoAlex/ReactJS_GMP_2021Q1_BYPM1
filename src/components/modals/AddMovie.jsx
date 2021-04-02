c6c26ba95d534f63552e3243aa5d67656b7d0bd6
Alexandr_Lyssenko <ALexandr>
2021-04-02 19:37:06 +0600

    feat: formik form

--- src/components/modals/AddMovie.jsx
+++ src/components/modals/AddMovie.jsx
@@ -1,28 +1,21 @@
 // eslint-disable-next-line max-len
 /* eslint-disable jsx-a11y/no-interactive-element-to-noninteractive-role,jsx-a11y/aria-role,jsx-a11y/label-has-associated-control,max-len */
 import React, { useEffect, useRef, useState } from "react";
-
+import {Formik, Form, Field, ErrorMessage, useFormik} from 'formik';
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import DropDown from "../utils/DropDown";
 import { createMovie, editMovie } from "../../redux/actions/actions";
 import ModalWindow from "../utils/ModalWindow";
+import * as Yup from "yup";
 
 const AddMovie = ({ onClose, movie, mode }) => {
-  // eslint-disable-next-line no-unused-vars
 
   const creationStatus = useSelector((state) => state.post_movie_desc);
   const errorBody = useSelector((state) => state.error_body);
   const genreList = useSelector((state) => state.genres);
 
-  const titleRef = useRef(movie.title);
-  const releaseDateRef = useRef(movie.release_date);
-  const posterPathRef = useRef(movie.poster_path);
-  const overviewRef = useRef(movie.overview);
-  const runtimeRef = useRef(movie.runtime);
-
   const [showModal, setShowModal] = useState(false);
-
   const dispatch = useDispatch();
 
   const [checkedItems, setCheckedItems] = useState(new Map());
@@ -45,26 +38,47 @@ const AddMovie = ({ onClose, movie, mode }) => {
     setShowModal(!showModal);
   };
 
-  const handleChange = (event) => {
-    event.preventDefault();
-
-    const movieToSave = {
-      id: movie.id,
-      title: titleRef.current.value,
-      release_date: releaseDateRef.current.value,
-      poster_path: posterPathRef.current.value,
-      overview: overviewRef.current.value,
-      runtime: Number.parseInt(runtimeRef.current.value, 10),
-      genres: [...checkedItems.keys()].filter((x) => checkedItems.get(x)).map((x) => x),
-    };
-
+  const saveMovie = (movieToSave) => {
+    console.log('movieToSave: ', movieToSave);
     if (mode !== undefined && mode === "EDIT") {
       dispatch(editMovie(movieToSave));
     } else {
+      delete movieToSave.id;
       dispatch(createMovie(movieToSave));
     }
     handleShowModal();
-  };
+  }
+
+
+  const formik = useFormik({
+    initialValues: {
+      title:movie.title === undefined ? "" : movie.title,
+      release_date:movie.release_date === undefined ? "" : movie.release_date,
+      poster_path:movie.poster_path === undefined ? "" : movie.poster_path,
+      overview: movie.overview === undefined ? "" : movie.overview,
+      runtime: movie.runtime === undefined ? "" : movie.runtime,
+      id:movie.id === undefined ? "" : movie.id,
+    },
+    validationSchema: Yup.object({
+      title:Yup.string().required("Title is required."),
+      release_date:Yup.date().required("Release date is required."),
+      poster_path:Yup.string().required("Poster path  is required."),
+      overview:Yup.string()
+          .min(5, 'Too short!')
+          .max(1000, 'Too long')
+          .required("Overview path  is required."),
+      runtime:Yup.number()
+          .positive()
+          .integer("It must be a whole number")
+          .required("Runtime is required."),
+    }),
+    onSubmit: values => {
+      values.genres = [...checkedItems.keys()].filter((x) => checkedItems.get(x)).map((x) => x);
+      saveMovie(values)
+    }
+  });
+
+
 
   return (
     <>
@@ -74,39 +88,48 @@ const AddMovie = ({ onClose, movie, mode }) => {
         </span>
         <h3>ADD MOVIE</h3>
         <div className="content">
-          <form>
+
+          <form onSubmit={formik.handleSubmit}>
             <section>
               <label htmlFor="title">
                 <span>TITLE</span>
                 <input
-                  type="text"
-                  id="title"
-                  placeholder="Enter movie title"
-                  ref={titleRef}
-                  defaultValue={movie.title}
+                    type="text"
+                    name="title"
+                    value={formik.values.title}
+                    onChange={formik.handleChange}
                 />
+                {formik.errors.title && formik.touched.title && (
+                    <p>{formik.errors.title}</p>
+                )}
               </label>
             </section>
             <section>
-              <label htmlFor="title">
-                <span>RELEASE DATE</span>
+              <label htmlFor="release_date">
+                <span>Release Date</span>
                 <input
-                  type="date"
-                  placeholder="dd-mm-yyyy"
-                  ref={releaseDateRef}
-                  defaultValue={movie.release_date}
+                    type="date"
+                    name="release_date"
+                    value={formik.values.release_date}
+                    onChange={formik.handleChange}
                 />
+                {formik.errors.release_date && formik.touched.release_date && (
+                    <p>{formik.errors.release_date}</p>
+                )}
               </label>
             </section>
             <section>
-              <label htmlFor="title">
-                <span>MOVIE URL</span>
+              <label htmlFor="poster_path">
+                <span>Movie URL</span>
                 <input
-                  type="text"
-                  placeholder="Enter movie URL"
-                  ref={posterPathRef}
-                  defaultValue={movie.poster_path}
+                    type="text"
+                    name="poster_path"
+                    value={formik.values.poster_path}
+                    onChange={formik.handleChange}
                 />
+                {formik.errors.poster_path && formik.touched.poster_path && (
+                    <p>{formik.errors.poster_path}</p>
+                )}
               </label>
             </section>
             <section className="drop_down_container">
@@ -119,37 +142,37 @@ const AddMovie = ({ onClose, movie, mode }) => {
               />
             </section>
             <section>
-              <label htmlFor="title">
+              <label htmlFor="overview">
                 <span>OVERVIEW</span>
                 <input
-                  type="text"
-                  placeholder="Overview here"
-                  ref={overviewRef}
-                  defaultValue={movie.overview}
+                    type="text"
+                    name="overview"
+                    value={formik.values.overview}
+                    onChange={formik.handleChange}
                 />
+                {formik.errors.overview && formik.touched.overview && (
+                    <p>{formik.errors.overview}</p>
+                )}
               </label>
             </section>
             <section>
-              <label htmlFor="title">
+              <label htmlFor="runtime">
                 <span>RUNTIME</span>
                 <input
-                  type="number"
-                  placeholder="Runtime here"
-                  ref={runtimeRef}
-                  defaultValue={movie.runtime}
-                  min="0"
-                  max="10000"
+                    type="number"
+                    name="runtime"
+                    value={formik.values.runtime}
+                    onChange={formik.handleChange}
                 />
+                {formik.errors.runtime && formik.touched.runtime && (
+                    <p>{formik.errors.runtime}</p>
+                )}
               </label>
             </section>
+            <button type="submit">Submit</button>
+            <button type="submit" onClick={onClose}>Reset</button>
           </form>
         </div>
-        <button role="search" type="button" onClick={onClose}>
-          Reset
-        </button>
-        <button role="reset" type="button" onClick={handleChange}>
-          Submit
-        </button>
       </section>
       <ModalWindow
         title="Movie creation result:"
