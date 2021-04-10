455081ac89820e04ebddf78db15571f4f6bcf0b6
Alexandr_Lyssenko <ALexandr>
2021-04-10 23:21:33 +0600

    fix: search by url

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router';
+import { useLocation } from 'react-router-dom';
 import Navigation from './Navigation';
 import ListOfMovies from './lists/ListOfMovies';
 import Search from './header/Search';
@@ -12,11 +12,12 @@ const SearchResult = () => {
   const searchMovie = useSelector((state) => state.search_movie);
   const dispatch = useDispatch();
 
-  const { query } = useParams();
+  const router = useLocation();
 
   useEffect(() => {
-    if (query !== undefined && query !== '' && searchMovie === '') {
-      dispatch(fetchPosts(query));
+    const search = router.pathname.split('/');
+    if (search !== undefined && search.length === 3 && searchMovie === '') {
+      dispatch(fetchPosts(search[2]));
     }
   }, []);
 
