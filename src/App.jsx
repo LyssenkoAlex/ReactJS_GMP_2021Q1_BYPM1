c5beaa77aa136bacd836f6c905efb367737072c0
Alexandr_Lyssenko <ALexandr>
2021-05-24 19:49:33 +0600

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './sass/style.scss';
 import {
-    GENRE_FILTER,
-    MOVIE_DETAILS_PAGE,
-    MOVIE_NOT_FOUND,
-    SEARCH_PAGE,
-    WELCOME_PAGE,
+  GENRE_FILTER,
+  MOVIE_DETAILS_PAGE,
+  MOVIE_NOT_FOUND,
+  SEARCH_PAGE,
+  WELCOME_PAGE,
 } from './components/utils/ROUTES';
 import Footer from './components/Footer';
 import ErrorBoundary from './components/error/ErrorBoundary';
@@ -14,25 +16,25 @@ import Welcome from './components/welcome/Welcome';
 import SearchResult from './components/SearchResult';
 import MovieDetails from './components/MovieDetails';
 import MovieNotFound from './components/MovieNotFound';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 const App = () => (
-    <>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path={WELCOME_PAGE.path} component={Welcome}/>
-                <Route path={MOVIE_NOT_FOUND.path} component={MovieNotFound}/>
-                <Route path={GENRE_FILTER.path} component={SearchResult}/>
-                <Route path={`${SEARCH_PAGE.path}/:query`} component={SearchResult}/>
-                <Route path={`${MOVIE_DETAILS_PAGE.path}/:id`} component={MovieDetails}/>
-                <Route path="/404" component={NotFound}/>
-                <Route component={NotFound}/>
-            </Switch>
-        </BrowserRouter>
-        <ErrorBoundary>
-            <Footer/>
-        </ErrorBoundary>
-    </>
+  <>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={WELCOME_PAGE.path} component={Welcome} />
+        <Route path={MOVIE_NOT_FOUND.path} component={MovieNotFound} />
+        <Route path={GENRE_FILTER.path} component={SearchResult} />
+
+        <Route path={`${SEARCH_PAGE.path}/:query`} component={SearchResult} />
+        <Route path={`${MOVIE_DETAILS_PAGE.path}/:id`} component={MovieDetails} />
+        <Route path="/404" component={NotFound} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+    <ErrorBoundary>
+      <Footer />
+    </ErrorBoundary>
+  </>
 );
 
 export default App;
